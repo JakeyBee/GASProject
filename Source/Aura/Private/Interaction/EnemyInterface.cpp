dacae60636e2a474528682aff85fdb68f90eb8af
Jake Burnett <jakeyb7410@gmail.com>
2024-09-15 16:42:35 -0500

    Basic setup of assets

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Unreal Engine - Gameplay Ability System - Top Down RPG by Stephen Ulibarri
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Unreal Engine - Gameplay Ability System - Top Down RPG by Stephen Ulibarri
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
