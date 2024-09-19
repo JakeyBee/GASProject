fe329811081c8a9ed66997b00f5e11a73d4fc453
Jake Burnett <jakeyb7410@gmail.com>
2024-09-19 09:59:13 -0500

    RPG UI

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -6,6 +6,10 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Player/AuraPlayerState.h"
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "Player/AuraPlayerController.h"
+#include "UI/HUD/AuraHUD.h"
+
 
 AAuraCharacter::AAuraCharacter()
 {
@@ -33,7 +37,6 @@ void AAuraCharacter::OnRep_Controller()
 	Super::OnRep_Controller();
 	// Init ability actor info for the client
 	InitAbilityActorInfo();
-	
 }
 
 void AAuraCharacter::InitAbilityActorInfo()
@@ -43,4 +46,12 @@ void AAuraCharacter::InitAbilityActorInfo()
 	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
 	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
 	AttributeSet = AuraPlayerState->GetAttributeSet();
-}
+
+	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(AuraPlayerController->GetHUD()))
+		{
+			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
+		}
+	}
+};
---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,37 @@
+// Unreal Engine - Gameplay Ability System - Top Down RPG by Stephen Ulibarri
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+
+
+UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetControllerParams& WCParams)
+{
+	if (OverlayWidgetController == nullptr)
+	{
+		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
+		OverlayWidgetController->SetWidgetControllerParams(WCParams);
+		OverlayWidgetController->BindCallbacksToDependencies();
+		return OverlayWidgetController;
+	}
+	
+	return OverlayWidgetController;
+}
+
+void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
+{
+    checkf(OverlayWidgetClass, TEXT("Overlay Widget Class Uninitialized. Please fill out BP_AuraHUD"));
+    checkf(OverlayWidgetControllerClass, TEXT("Over Widget Controller Class Uninitialized. Please fill out BP_AuraHUD"));
+	
+    UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	OverlayWidget = Cast<UAuraUserWidget>(Widget);
+
+	const FWidgetControllerParams WidgetControllerParams(PC,PS, ASC, AS);
+	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
+
+	OverlayWidget->SetWidgetController(WidgetController);
+	WidgetController->BroadCastInitialValue();
+    Widget->AddToViewport();
+}
+
